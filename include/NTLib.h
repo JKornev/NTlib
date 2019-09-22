aa527e12e592a6fca1d3117f8263feccb98240c7
Jora Kornev <8bit.dosninja@gmail.com>
2019-09-22 20:19:12 +0300

    NtlPs routines template

--- include/NTLib.h
+++ include/NTLib.h
@@ -2,3 +2,18 @@
 
 #include "NTExp.h"
 
+NTSTATUS NtlPsCreateProcess(
+    _Out_    PHANDLE Handle,
+    _In_     LPWSTR Path,
+    _In_opt_ LPWSTR CommandLine,
+    _In_     ACCESS_MASK Access,
+    _In_     BOOLEAN InheritObjects,
+    _In_opt_ POBJECT_ATTRIBUTES Attributes = NULL
+);
+
+NTSTATUS NtlPsOpenProcess(
+    _Out_    PHANDLE Handle,
+    _In_     DWORD ProcessID,
+    _In_     ACCESS_MASK Access,
+    _In_opt_ POBJECT_ATTRIBUTES Attributes = NULL
+);
